67397e4420475eb4425953dc0162cb43eb87c6b6
Artur Skrzydlo <artur.skrzydlo@deliveroo.com>
2022-12-28 22:41:45 +0100

    added build tools - initial setup with linters on

---
+++ Makefile
@@ -0,0 +1,42 @@
+# Install all development tools and build artifacts to the project's
+# `bin` directory.
+export GOBIN=$(CURDIR)/bin
+
+# Ensure that all dependencies are installed using vendored sources (/vendor).
+export GOFLAGS=-mod=vendor
+
+# Default to the system 'go'.
+GO?=$(shell which go)
+
+$(GOBIN):
+	mkdir -p $(GOBIN)
+
+.PHONY: setup
+setup: ## Setting up local env
+	cd $(GOBIN)
+	wget -O- -nv https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s v1.50.1
+
+.PHONY: clean
+clean: ## Remove build artifacts.
+	rm -rf $(GOBIN)
+
+.PHONY: lint
+lint: ## Lint the source code.
+	$(GOBIN)/golangci-lint run --config $(shell pwd)/build/.golangci.yml --verbose ./...
+
+.PHONY: integration-test
+integration-test: ## Run all integration tests
+	$(GO) test -v -tags=integration ./...
+
+.PHONY: cover
+cover: ## Calculate coverage
+	@go test -mod=vendor -coverprofile=coverage.out ./... ; \
+	cat coverage.out | \
+	awk 'BEGIN {cov=0; stat=0;} $$3!="" { cov+=($$3==1?$$2:0); stat+=$$2; } \
+	END {printf("Total coverage: %.2f%% of statements\n", (cov/stat)*100);}'
+	@go tool cover -html=coverage.out
+
+.PHONY: tidy
+tidy: ## Tidy go modules and re-vendor
+	@go mod tidy
+	@go mod vendor
--- api/client.go
+++ api/client.go
@@ -15,8 +15,8 @@ const (
 
 type AccountClient interface {
 	CreateAccount(ctx context.Context, accountData *pkg.AccountData) error
-	FetchAccount(ctx context.Context, accountId string) (account *pkg.AccountData, err error)
-	DeleteAccount(ctx context.Context, accountId string) error
+	FetchAccount(ctx context.Context, accountID string) (account *pkg.AccountData, err error)
+	DeleteAccount(ctx context.Context, accountID string) error
 }
 
 type Client struct {
@@ -27,24 +27,25 @@ type Client struct {
 }
 
 func (c *Client) CreateAccount(ctx context.Context, accountData *pkg.AccountData) error {
-	//TODO implement me
+	// TODO implement me
 	panic("implement me")
 }
 
 func (c *Client) FetchAccount(ctx context.Context, accountId string) (account *pkg.AccountData, err error) {
-	//TODO implement me
+	// TODO implement me
 	panic("implement me")
 }
 
 func (c *Client) DeleteAccount(ctx context.Context, accountId string) error {
-	//TODO implement me
+	// TODO implement me
 	panic("implement me")
 }
 
 func NewClient(apiKey string) *Client {
 	logger, _ := zap.NewProduction()
 
-	return &Client{apiKey: apiKey,
+	return &Client{
+		apiKey:     apiKey,
 		baseURL:    baseURLV1,
 		httpClient: &http.Client{Timeout: time.Second * 20},
 		logger:     logger,
