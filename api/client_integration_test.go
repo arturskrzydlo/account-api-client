643d8c7ffb4d64b66215e9fd397549d0238b6469
Artur Skrzydlo <artur.skrzydlo@deliveroo.com>
2022-12-29 12:04:37 +0100

    implemented test for happy path account creation

--- api/client_integration_test.go
+++ api/client_integration_test.go
@@ -27,7 +27,17 @@ func (s *accountApiClientSuite) SetupSuite() {
 
 func (s *accountApiClientSuite) TestCreateAccount() {
 	s.Run("should successfully create single account", func() {
-		s.Assert().NotNil(s.accountApiClient.CreateAccount(context.Background(), &models.AccountData{}))
+		// given
+		account := createAccount("test-account-id", "test-organization-id")
+
+		// when
+		err := s.accountApiClient.CreateAccount(context.Background(), account)
+
+		// then
+		s.Assert().NoError(err)
+		fetchedAccount, err := s.accountApiClient.FetchAccount(context.Background(), account.ID)
+		s.Assert().NoError(err)
+		s.assertCreatedAccount(account, fetchedAccount)
 	})
 }
 
@@ -41,4 +51,62 @@ func (s *accountApiClientSuite) TestDeleteAccount() {
 	s.Run("should successfully delete single account", func() {
 		s.Assert().NotNil(s.accountApiClient.FetchAccount(context.Background(), "account-id"))
 	})
+}
+
+func createAccount(accountID string, organizationID string) *models.AccountData {
+	version := new(int64)
+	*version = 0
+	accountClassification := "Personal"
+	accountMatchingOptOut := false
+	country := "GB"
+	jointAccount := false
+
+	return &models.AccountData{
+		Attributes: &models.AccountAttributes{
+			AccountClassification:   &accountClassification, // enum ?
+			AccountMatchingOptOut:   &accountMatchingOptOut, // deprecated
+			AccountNumber:           "41426819",
+			AlternativeNames:        []string{"Sam Holder"},
+			BankID:                  "400300",
+			BankIDCode:              "GBDSC",
+			BaseCurrency:            "GBP",
+			Bic:                     "NWBKGB22",
+			Country:                 &country,
+			Iban:                    "GB11NWBK40030041426819", // generated if not provided
+			JointAccount:            &jointAccount,
+			Name:                    []string{"Samantha Holder"},
+			SecondaryIdentification: "A1B2C3D4",
+			Status:                  nil, // Status of the account. pending and confirmed are set by Form3, closed can be set manually. Test creating closed account
+			Switched:                nil, // deprecated, account switched away from organization
+		},
+		ID:             accountID,
+		OrganisationID: organizationID,
+		Type:           "accounts",
+		Version:        version, // incremented witch each update, probably not needed in create
+	}
+}
+
+func (s *accountApiClientSuite) assertCreatedAccount(expectedAccount *models.AccountData, actualAccount *models.AccountData) {
+	s.Assert().Equal(expectedAccount.ID, actualAccount.ID)
+	s.Assert().Equal(expectedAccount.Type, actualAccount.Type)
+	s.Assert().Equal(expectedAccount.Version, actualAccount.Version)
+	s.Assert().Equal(expectedAccount.OrganisationID, actualAccount.OrganisationID)
+	// asserting account attributes
+	s.Assert().Equal(expectedAccount.Attributes.AccountClassification, actualAccount.Attributes.AccountClassification)
+	s.Assert().Equal(expectedAccount.Attributes.AccountMatchingOptOut, actualAccount.Attributes.AccountMatchingOptOut)
+	s.Assert().Equal(expectedAccount.Attributes.AccountNumber, actualAccount.Attributes.AccountNumber)
+	s.Assert().Equal(expectedAccount.Attributes.AlternativeNames[0], actualAccount.Attributes.AlternativeNames[0])
+	s.Assert().Equal(expectedAccount.Attributes.BankID, actualAccount.Attributes.BankID)
+	s.Assert().Equal(expectedAccount.Attributes.BankIDCode, actualAccount.Attributes.BankIDCode)
+	s.Assert().Equal(expectedAccount.Attributes.BaseCurrency, actualAccount.Attributes.BaseCurrency)
+	s.Assert().Equal(expectedAccount.Attributes.Bic, actualAccount.Attributes.Bic)
+	s.Assert().Equal(expectedAccount.Attributes.Country, actualAccount.Attributes.Country)
+
+	if expectedAccount.Attributes.Iban != "" {
+		s.Assert().Equal(expectedAccount.Attributes.Iban, actualAccount.Attributes.Iban)
+	}
+
+	s.Assert().Equal(expectedAccount.Attributes.JointAccount, actualAccount.Attributes.JointAccount)
+	s.Assert().Equal(expectedAccount.Attributes.Name[0], actualAccount.Attributes.Name[0])
+	s.Assert().Equal(expectedAccount.Attributes.SecondaryIdentification, actualAccount.Attributes.SecondaryIdentification)
 }
